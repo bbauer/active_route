ad3037d53f2a2e5d90534afc641941c8a3a1144c
Brian Bauer <brian.bauer@g5platform.com>
2011-05-30 12:23:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouteManager::Application.config.secret_token = 'd9b1c38c1bdebd87ca72ac6411caf8af275069a21479f628df8a5a4e5a265a9a50313ba595e3a40ac9e07a751713330f329cc6deb0fb53518261a9956b50cbe4'
