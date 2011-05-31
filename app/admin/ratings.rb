f9a2886584e457c401098c12e39c640af69fd40b
Brian Bauer <brian.bauer@g5platform.com>
2011-05-31 08:30:45 -0700

    Remove ratings model from admin

--- app/admin/ratings.rb
+++
@@ -1,3 +0,0 @@
-ActiveAdmin.register Rating do
-  
-end
--- app/admin/setters.rb
+++ app/admin/setters.rb
@@ -1,3 +1,8 @@
 ActiveAdmin.register Setter do
-
+  index do
+    id_column
+    column :name
+    column :created_at
+    default_actions
+  end
 end
